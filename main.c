2b74e66d6985df0239ec69098b6147c842514be7
Sebastien Neyt <sneyt@shi-r9-p5.s19.be>
2022-05-11 12:00:29 +0200

    norminette

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sneyt <sneyt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 08:37:17 by sneyt             #+#    #+#             */
-/*   Updated: 2022/05/11 11:54:22 by sneyt            ###   ########.fr       */
+/*   Updated: 2022/05/11 11:59:48 by sneyt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 int	main(void)
 {
-    int     fd;
-    char    *line;
+	int		fd;
+	char	*line;
 
-    fd = open("test.txt", O_RDONLY);
-    while (1)
-    {
-        line = get_next_line(fd);
-        if (line == NULL)
-            break;
-        printf("%s", line);
-        free(line);
-    }
-    return (0);
+	fd = open("test.txt", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
